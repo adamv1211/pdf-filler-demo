670ab1e7b20da0cfb7c8ac6530fa57cdeffd4292
adam <adamv1211@gmail.com>
2026-02-06 21:11:01 -0500

    added checkbox functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pypdf import PdfReader, PdfWriter
+from pypdf.generic import NameObject, BooleanObject
 import json
-import os
+import os, time
 from get_customer import get_customer_info
 #######################################################
 # ALL NAMES AND DATA ARE FAKE AND FOR DEMO PURPOSES ONLY!!!
@@ -10,11 +11,12 @@ from get_customer import get_customer_info
 def main():
     print("You have started pdf filler")
     blank_pdf = PdfReader("test_spi.pdf")
+    #blank_pdf = PdfReader("CHECKEDtest_spi.pdf")
     writer = PdfWriter()
     writer.append(blank_pdf)
 
 #Finds field names and values for mapping
-    for page in blank_pdf.pages:
+    for page in writer.pages:
         Annots = page.get("/Annots")
         if not Annots:
             continue
@@ -22,8 +24,15 @@ def main():
             field = Annot_ref.get_object()
             name = field.get("/T", "Unnamed")
             value = field.get("/V", "")
+            ap = field.get("/AP", "")
             ftype = field.get("/FT", "")
-            print(f"{name:25} | value = {value:10} | type= {ftype}")
+            print(f"{name:25} | value = {value:10} | AP = {ap}| type= {ftype}")
+            #
+            if name == "Ibp":
+                field.update({
+                    NameObject("/V"):  NameObject("/Yes"),
+                    NameObject("/AS"): NameObject("/Yes"),
+                })
 
     with open('customers.json', 'r') as file:
         clients = json.load(file)
@@ -40,6 +49,9 @@ def main():
     mi = middle[0] if middle else ""
     print(f"Filling pdf for {first} {mi} {last}")
   
+    #hardcoded choice for testing
+    instruction_type = "Ibp"
+
     field_map = {
         "FirstName": first,
         "LastName": last,
@@ -51,17 +63,43 @@ def main():
         "RoutingNumber": client.get("routing_num"),
         "NameOfBank": client.get("bank_name"),
         "NamesOnAccount": ", ".join(client.get("names_on_account", [])),
-        "FullName": full_name
+        "FullName": full_name,
+        "Ibp": "Ibp",
+        "1st party Etf": "1st party Etf",
+        "Third party etf": "Third party etf"
     }
 
     
-    
     for page in writer.pages:
         writer.update_page_form_field_values(page, field_map)
+    
+        
+    
+    for page in writer.pages:
+        Annots = page.get("/Annots")
+        if not Annots:
+            continue
+        for Annot_ref in Annots:
+            field = Annot_ref.get_object()
+            name = field.get("/T", "Unnamed")
+            if name == "Ibp":
+                field.update({NameObject("/V"): NameObject("/Yes"), NameObject("/AS"): NameObject("/Yes")})
+            value = field.get("/V", "")
+            ftype = field.get("/FT", "")
+            print(f"{name:25} | value = {value:10}| type= {ftype}")
+    if "/AcroForm" in writer._root_object:
+        writer._root_object["/AcroForm"][NameObject("/NeedAppearances")] = BooleanObject(True)
+        
+
+
     with open(f"{first}_{last}_filled.pdf", "wb") as f:
         writer.write(f)
 
+    #Opens Pdf after generation
+    #DOES NOT DISPLAY CHECK MARKS IF OPENED IN THIS WAY
+    os.system(f"pkill okular")
     os.system(f"xdg-open {first}_{last}_filled.pdf")
+    
 
 
 
